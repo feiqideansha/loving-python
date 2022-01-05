2f77e845ceaebaaa6decceeda2178147cb5f8b7c
feiqideansha <94638016+feiqideansha@users.noreply.github.com>
2022-01-05 13:04:24 +0000

    dj38 py file
    dj38 py file

--- dj38-code.py
+++ dj38-code.py
@@ -3,8 +3,8 @@ import openpyxl
 import time
 start = time.time()
 
-input_file = "D:\我爱学习\Computational Methods for Logistics\Assignment\Coding\dj38.xlsx"
-input_file_profit = "D:\我爱学习\Computational Methods for Logistics\Assignment\Coding\dj38_profit.xlsx"
+input_file = "dj38.xlsx" #Please enter the correct file path here.
+input_file_profit = "dj38_profit.xlsx" #Please enter the correct file path here.
 excel = openpyxl.load_workbook(input_file)
 excel_profit = openpyxl.load_workbook(input_file_profit)
 sheet = excel.active
